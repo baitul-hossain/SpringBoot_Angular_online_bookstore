5d2bdd95f6ffd56e8652763f51b3cdd561b87fc9
baitul-hossain <baitul.hossain@gmail.com>
2021-08-13 23:43:38 +0100

    sercurity implemented

--- src/main/java/com/onlinelibrary/sercurity/CustomUserDetailsService.java
+++ src/main/java/com/onlinelibrary/sercurity/CustomUserDetailsService.java
@@ -1,5 +1,35 @@
 package com.onlinelibrary.sercurity;
 
-public class CustomUserDetailsService {
-    //New changes
+import com.onlinelibrary.model.User;
+import com.onlinelibrary.service.IUserService;
+import com.onlinelibrary.util.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username).
+                orElseThrow(() -> new UsernameNotFoundException(username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user).id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
 }
